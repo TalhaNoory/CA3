9fb762f3a768ed4507eadac1776d4c1f0167c2b7
TalhaNoory <cph-an148@cphbusiness.dk>
2019-11-17 14:15:23 +0100

    first commit

---
+++ target/generated-sources/annotations/entities/Output_.java
@@ -0,0 +1,15 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-14T13:25:51")
+@StaticMetamodel(Output.class)
+public class Output_ { 
+
+    public static volatile SingularAttribute<Output, Integer> id;
+    public static volatile SingularAttribute<Output, String> title;
+    public static volatile SingularAttribute<Output, String> body;
+
+}
